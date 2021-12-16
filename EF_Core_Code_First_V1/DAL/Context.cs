c835ae6d4895fb224a0731b39bbee4b00d33f64a
Lucas Juan <ljgsodre@outlook.com>
2021-12-16 13:35:53 -0300

    Adicionar arquivos de projeto.

---
+++ EF_Core_Code_First_V1/DAL/Context.cs
@@ -0,0 +1,21 @@
+﻿using EF_Core_Code_First_V1.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_Code_First_V1.DAL
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=StoreDB;User Id=sa;Password=qwe123;");
+        }
+    }
+}
---
+++ EF_Core_Code_First_V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using EF_Core_Code_First_V1.DAL;
+using System;
+using System.Linq;
+
+namespace EF_Core_Code_First_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new MyContext();
+            Console.WriteLine("Costumer no banco: " + obj.Orders.Where(x => x.CustomerID == 1).Select( y => y.CustomerID).FirstOrDefault());
+        }
+    }
+}
